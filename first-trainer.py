fe50a9b28d3d035e3e084eacad25c288d39a0f88
Raúl Solera <raul.solera.rallo@gmail.com>
2017-10-23 23:48:41 +0200

    20171023_6

--- first-trainer.py
+++ first-trainer.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 # Load csv log file
 lines = []
@@ -26,6 +26,7 @@ y_train = np.array(measurements)
 
 # Define simple model
 model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160, 320, 3)))
 model.add(Flatten(input_shape= (160, 320, 3)))
 model.add(Dense(1))
 
