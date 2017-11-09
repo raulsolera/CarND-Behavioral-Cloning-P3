10a05cea77baf823afadc74b65ae78757aca2046
Raúl Solera <raul.solera.rallo@gmail.com>
2017-11-09 20:23:59 +0100

    20171109-v3

--- helper.py
+++ helper.py
@@ -241,7 +241,7 @@ def generate_next_batch(batch_size=BATCH_SIZE):
         yield np.array(X_batch), np.array(y_batch)
 
 
-def save_model(model, model_name='model.json', weights_name='model.h5'):
+def save_model(model, model_name = 'model.h5', json_model_name='model.json', weights_name='model_w.h5'):
     """
     Save the model into the hard disk
 
@@ -257,14 +257,15 @@ def save_model(model, model_name='model.json', weights_name='model.h5'):
     :return:
         None
     """
-    silent_delete(model_name)
+    silent_delete(json_model_name)
     silent_delete(weights_name)
 
     json_string = model.to_json()
-    with open(model_name, 'w') as outfile:
+    with open(json_model_name, 'w') as outfile:
         json.dump(json_string, outfile)
 
     model.save_weights(weights_name)
+    model.save(model_name)
 
 
 def silent_delete(file):
