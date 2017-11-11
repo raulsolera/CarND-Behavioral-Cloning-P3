e16db27993582923239cc1b98ea841df3f81406e
Raúl Solera <raul.solera.rallo@gmail.com>
2017-11-11 10:00:27 +0100

    20171111-v2

--- model.py
+++ model.py
@@ -1,77 +1,91 @@
-import tensorflow as tf
-from keras.layers import Dense, Flatten, Lambda, Activation, MaxPooling2D
-from keras.layers.convolutional import Convolution2D
-from keras.models import Sequential
+#\---> IMPORTS <-------------------------------------------------------------\#
+# Modules
+import numpy as np
+import csv
+
+# utils
+from sklearn.model_selection import train_test_split
+from utils import transformed_data_generator
+
+# Keras
+from keras.models import Sequential, load_model
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Dropout, MaxPooling2D
 from keras.optimizers import Adam
+from keras.regularizers import l2
 
-import helper
 
-# tf.python.control_flow_ops = tf
+#\---> CONSTANTS <-----------------------------------------------------------\#
+# Data
+DATA_DIR = 'data/'
+CSV_FILE = 'driving_log.csv'
 
-number_of_epochs = 8
-number_of_samples_per_epoch = 20032
-number_of_validation_samples = 6400
-learning_rate = 1e-4
-activation_relu = 'relu'
+# batch size
+BATCH_SIZE = 32
 
-# Our model is based on NVIDIA's "End to End Learning for Self-Driving Cars" paper
-# Source:  https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
-model = Sequential()
 
-model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(64, 64, 3)))
+#\---> LOAD AND SPLIT DATA <-------------------------------------------------\#
+# Load csv log file
+csv_file = []
+with open(DATA_DIR+CSV_FILE) as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)  # skip the headers
+    for line in reader:
+        csv_file.append(line)
 
-# starts with five convolutional and maxpooling layers
-model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
-model.add(Activation(activation_relu))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
-model.add(Activation(activation_relu))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+csv_file = csv_file[:100]
+# Split csv in train and validation sets
+csv_train, csv_valid = train_test_split(csv_file, test_size = 0.2, shuffle = True)
 
-model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
-model.add(Activation(activation_relu))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
-model.add(Activation(activation_relu))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+#\---> CREATE AND TRAIN MODEL <----------------------------------------------\#
+keep_prob = .7
+def create_nv_model():
+    # Define nvidia model
+    model = Sequential()
 
-model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
-model.add(Activation(activation_relu))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(64, 64, 3)))
 
-model.add(Flatten())
+    model.add(Conv2D(24, (5, 5), padding='same', strides=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-# Next, five fully connected layers
-model.add(Dense(1164))
-model.add(Activation(activation_relu))
+    model.add(Conv2D(36, (5, 5), padding='same', strides=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-model.add(Dense(100))
-model.add(Activation(activation_relu))
+    model.add(Conv2D(48, (5, 5), padding='same', strides=(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-model.add(Dense(50))
-model.add(Activation(activation_relu))
+    model.add(Conv2D(64, (3, 3), padding='same', strides=(1, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-model.add(Dense(10))
-model.add(Activation(activation_relu))
+    model.add(Conv2D(64, (3, 3), padding='same', strides=(1, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-model.add(Dense(1))
+    model.add(Flatten())
 
-model.summary()
+    model.add(Dense(1164, activation='relu'))
+    model.add(Dropout(keep_prob))
 
-model.compile(optimizer=Adam(learning_rate), loss="mse", )
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(keep_prob))
 
-# create two generators for training and validation
-train_gen = helper.generate_next_batch()
-validation_gen = helper.generate_next_batch()
+    model.add(Dense(50, activation='relu'))
+    model.add(Dropout(keep_prob))
 
-history = model.fit_generator(train_gen,
-                              steps_per_epoch = number_of_samples_per_epoch / helper.BATCH_SIZE,
-                              nb_epoch = number_of_epochs,
-                              validation_data = validation_gen,
-                              validation_steps = number_of_validation_samples / helper.BATCH_SIZE,
-                              verbose=1)
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
 
-# finally save our model and weights
-helper.save_model(model)
+    model.compile(loss='mse', optimizer=Adam())
+    return model
+
+
+nv_model = create_nv_model()
+nv_hist = nv_model.fit_generator(transformed_data_generator(csv_train),
+                                steps_per_epoch = len(csv_train)/BATCH_SIZE*3,
+                                epochs=8,
+                                validation_data = transformed_data_generator(csv_valid),
+                                validation_steps = len(csv_valid)/BATCH_SIZE*3)
+
+
+# Save model
+nv_model.save('models/nv_model.h5')
--- utils.py
+++ utils.py
@@ -1,14 +1,166 @@
-# Imports
+#\---> IMPORTS <-------------------------------------------------------------\#
+import numpy as np
 import cv2
+import os, errno
+from scipy.ndimage import rotate
+from sklearn.utils import shuffle
+from imgaug import augmenters as iaa
 
-# Cropping parameters
-TOP_STRIDE_TO_CROP = 30
-BOTTOM_STRIDE_TO_CROP = 25
+
+#\---> CONSTANTS <-----------------------------------------------------------\#
+# Data
+DATA_DIR = 'data/'
+CSV_FILE = 'driving_log.csv'
+
+# Resize constants
+TOP_CUT = 30
+BOTTOM_CUT = 30
 NEW_WIDTH = 64
 NEW_HEIGHT = 64
+MAX_ROTATION_ANGLE = 15
+MAX_SHEAR_SHIFT = 40
+STEERING_CORRECTION = 0.23
 
-# crop resize funtion to in both training and driving
-def crop_resize(image):
-    height = image.shape[0]
-    cropped_image = image[TOP_STRIDE_TO_CROP:height-BOTTOM_STRIDE_TO_CROP, :, :]
-    return cv2.resize(cropped_image,(NEW_WIDTH, NEW_HEIGHT), interpolation=cv2.INTER_AREA)
+# batch size
+BATCH_SIZE = 32
+
+
+#\---> IMAGE TRANSFORMATIONS <-----------------------------------------------\#
+### Image transformations
+# Crop image
+def crop(image, top_cut=TOP_CUT, bottom_cut=BOTTOM_CUT, left_cut=0, right_cut=0):
+    """
+    Crop top, bottom, left and right sides of image
+    """
+    height, width = image.shape[0:2]
+    cropped_image = image[top_cut:height - bottom_cut, left_cut:width - right_cut, :]
+    return cropped_image
+
+
+# Resize image
+def resize(image, new_width=NEW_WIDTH, new_height=NEW_HEIGHT):
+    """
+    Resize image to new size
+    """
+    return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+
+# Random flip
+def random_flip(image, angle):
+    """
+    Random flip image horizontally with probability of 0.5
+    """
+    if np.random.random() > 0.5:
+        image = cv2.flip(image, 1)
+        angle *= -1.
+    return image, angle
+
+
+# Random brightnes correction
+def random_brightness_correction(image, angle):
+    """
+    Correct brightness of image in a random factor from (0.3, 1.7) in all channels
+    """
+    brightness_factor = np.random.uniform(0.4, 1.6)
+    image = image * brightness_factor
+    image[image > 255] = 255
+    image = np.array(image, dtype=np.uint8)
+    return image, angle
+
+
+# Random shear
+def random_shear(image, angle, shear_range=MAX_SHEAR_SHIFT):
+    """
+    Shear (affine transformation) horizontally by a random shift in the range of shear_range
+    Implies also steering angle correction
+    https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/warp_affine/warp_affine.html
+    """
+    rows, cols, channels = image.shape
+    shear_shift = np.random.randint(-shear_range, shear_range + 1)
+    random_point = [cols / 2 + shear_shift, rows / 2]
+    pts1 = np.float32([[0, rows], [cols, rows], [cols / 2, rows / 2]])
+    pts2 = np.float32([[0, rows], [cols, rows], random_point])
+    M = cv2.getAffineTransform(pts1, pts2)
+    image = cv2.warpAffine(image, M, (cols, rows))
+    shear_angle = shear_shift / (rows / 2) * 180 / (np.pi * 25.0) / 6
+
+    return image, angle + shear_angle
+
+
+# Random rotation
+def random_rotate(image, angle, rotation_range=MAX_ROTATION_ANGLE):
+    """
+    Rotate image by a random angle in the range of rotation_range
+    Implies also steering angle correction
+    """
+    rotation_angle = np.random.uniform(-rotation_range, rotation_range+1)
+    rotate_fun = iaa.Affine(rotate=(-rotation_angle))
+    image = rotate_fun.augment_images([image])[0]
+    rad = (np.pi / 180.0) * rotation_angle
+
+    return image, angle - rad
+
+
+#\---> GENERATOR <-----------------------------------------------------------\#
+def transformed_data_generator(data, data_dir=DATA_DIR,
+                               batch_size=BATCH_SIZE,
+                               image_load=True):
+    # Camera parameters
+    cameras = ['center', 'left', 'right']
+    cameras_index = {'center': 0, 'left': 1, 'right': 2}  # 0:center, 1:left, 2:right
+    cameras_steering_correction = {'center': 0, 'left': STEERING_CORRECTION, 'right': -STEERING_CORRECTION}
+
+    num_samples = len(data)
+
+    while 1:  # Loop forever so the generator never terminates
+
+        data = shuffle(data)
+        images = []
+        angles = []
+
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = data[offset:offset + batch_size]
+
+            for line in batch_samples:
+
+                ### Randomly choose center, left or right image
+                # Get random camera 0:center, 1:left, 2:right
+                camera = np.random.choice(cameras)
+                file_name = line[cameras_index[camera]].split('/')[-1]
+                path = data_dir + 'IMG/' + file_name
+                if image_load:
+                    image = cv2.imread(path)
+                else:
+                    image = np.zeros((160, 320, 3), dtype=np.uint8)
+                # Adjust angle
+                angle = float(line[3])
+                angle += cameras_steering_correction[camera]
+
+                ### Random transformations
+                image, angle = random_flip(image, angle)
+                image, angle = random_brightness_correction(image, angle)
+                image, angle = random_shear(image, angle)
+                # image, angle = random_rotate(image, angle)
+
+                ### Resize
+                image = crop(image)
+                image = resize(image)
+
+                images.append(image)
+                angles.append(angle)
+
+            yield np.array(images), np.array(angles)
+
+
+#\---> MODEL SAVING <--------------------------------------------------------\#
+# Delete previous files:
+def delete_file(file_name):
+    try:
+        os.remove(file_name)
+    except OSError as error:
+        if error.errno != errno.ENOENT:
+            raise
+
+def save_model(model, model_name):
+    delete_file(model_name)
+    model.save(model_name)
